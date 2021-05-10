b26e7a1d00753f90e7566063ffd2c99f67eb2749
Horgeon <me@horgeon.fr>
2021-05-10 20:51:48 +0200

    Added a new abstraction for retrieving a parkour's location

--- src/main/java/fr/prodrivers/bukkit/parkouraddon/adaptation/Course.java
+++ src/main/java/fr/prodrivers/bukkit/parkouraddon/adaptation/Course.java
@@ -1,9 +1,19 @@
 package fr.prodrivers.bukkit.parkouraddon.adaptation;
 
+import io.github.a5h73y.parkour.Parkour;
 import io.github.a5h73y.parkour.type.course.CourseInfo;
+import org.bukkit.Location;
 
 public class Course {
 	static public void setMinimumLevel( String courseName, int level ) {
 		CourseInfo.setMinimumParkourLevel( courseName, level );
+	}
+
+	static public Location getLocation( String courseName ) {
+		io.github.a5h73y.parkour.type.course.Course course = Parkour.getInstance().getCourseManager().findCourse( courseName );
+		if( course == null ) {
+			return null;
+		}
+		return course.getCheckpoints().get( 0 ).getLocation();
 	}
 }
