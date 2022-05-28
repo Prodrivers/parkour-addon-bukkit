56c6a50cef98146929ce76fe596d3d44c8c40857
Horgeon <me@horgeon.fr>
2022-05-28 18:13:53 +0200

    Call database sync on player login

--- src/main/java/fr/prodrivers/bukkit/parkouraddon/listeners/PlayerLoginLogoutListener.java
+++ src/main/java/fr/prodrivers/bukkit/parkouraddon/listeners/PlayerLoginLogoutListener.java
@@ -1,8 +1,16 @@
 package fr.prodrivers.bukkit.parkouraddon.listeners;
 
+import fr.prodrivers.bukkit.parkouraddon.adaptation.Parkoins;
+import fr.prodrivers.bukkit.parkouraddon.adaptation.ParkourLevel;
+import fr.prodrivers.bukkit.parkouraddon.models.EStoredPlayer;
+import fr.prodrivers.bukkit.parkouraddon.plugin.EConfiguration;
 import fr.prodrivers.bukkit.parkouraddon.ui.ParkourSelection;
+import io.ebean.Database;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import javax.inject.Inject;
@@ -10,11 +18,32 @@ import javax.inject.Singleton;
 
 @Singleton
 public class PlayerLoginLogoutListener implements Listener {
+	private final Database database;
+	private final EConfiguration configuration;
+	private final Economy economy;
+	private final Parkoins parkoinsManager;
+	private final ParkourLevel parkourLevelManager;
 	private final ParkourSelection parkourSelectionUi;
 
 	@Inject
-	public PlayerLoginLogoutListener(ParkourSelection parkourSelectionUi) {
+	public PlayerLoginLogoutListener(Database database, EConfiguration configuration, Economy economy, Parkoins parkoinsManager, ParkourLevel parkourLevelManager, ParkourSelection parkourSelectionUi) {
+		this.database = database;
+		this.configuration = configuration;
+		this.economy = economy;
+		this.parkoinsManager = parkoinsManager;
+		this.parkourLevelManager = parkourLevelManager;
 		this.parkourSelectionUi = parkourSelectionUi;
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		final Player player = event.getPlayer();
+		new Thread(() -> {
+			EStoredPlayer storedPlayer = EStoredPlayer.get(this.database, player);
+			if(storedPlayer != null) {
+				storedPlayer.onLogin(player, this.configuration, this.parkoinsManager, this.parkourLevelManager, this.economy);
+			}
+		}).start();
 	}
 
 	@EventHandler
@@ -23,6 +52,7 @@ public class PlayerLoginLogoutListener implements Listener {
 	}
 
 	public void unregister() {
+		PlayerJoinEvent.getHandlerList().unregister(this);
 		PlayerQuitEvent.getHandlerList().unregister(this);
 	}
 }
